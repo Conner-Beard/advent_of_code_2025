c83aaa68b8fdbe7dda412a033f4d0c65c7095dc6
Conner Beard <connerbeard3@gmail.com>
2025-11-30 21:19:27 -0800

    day 1 start

---
+++ day1/main.py
@@ -0,0 +1,15 @@
+
+def get_input():
+    ...
+
+def solve():
+    ...
+
+if __name__ == '__main__':
+    print(f'Solving {__file__}')
+
+    value = solve('example_input.txt')
+    assert value == 3
+
+    value = solve('input.txt')
+    print(value)
